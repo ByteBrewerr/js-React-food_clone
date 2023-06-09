7c3538f192544c6415c536e5bf8dbec654471d94
ByteBrewerr <yarr.sova@mail.ru>
2023-06-09 21:23:31 +0300

    auth1

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Feedback = () => {
     return (
         <div className='text-white w-[1200px] m-auto'>
-            Тут будут отзывы
+            Тут будут отзывы1
         </div>
     );
 };
